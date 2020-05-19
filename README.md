56d977eaf715b97a45e3bc99a231670ac1e79839
Patrick Cate <patrickcate@users.noreply.github.com>
2020-05-18 23:57:34 -0400

    Add leaflet plugin vue component.

--- README.md
+++ README.md
@@ -1,24 +1,33 @@
 # vue-leaflet-minimap
 
+A [vue2-leaflet](https://github.com/vue-leaflet/Vue2Leaflet) wrapper for the [leaflet-minimap](https://github.com/Norkart/Leaflet-MiniMap) control plugin.
+
+# Installation
+
 ## Project setup
-```
+
+```bash
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
-```
+
+```bash
 yarn serve
 ```
 
 ### Compiles and minifies for production
-```
+
+```bash
 yarn build
 ```
 
 ### Lints and fixes files
-```
+
+```bash
 yarn lint
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+### Author
+
+[Patrick Cate](https://github.com/patrickcate)
---
+++ src/vue-leaflet-minimap.vue
@@ -0,0 +1,68 @@
+<script>
+import { DomEvent } from 'leaflet'
+import { findRealParent, propsBinder } from 'vue2-leaflet'
+import MiniMap from 'leaflet-minimap'
+
+export default {
+  name: 'VueLeafletMinimap',
+  props: {
+    layer: {
+      type: Object,
+      required: true,
+    },
+    options: {
+      type: Object,
+      default() {
+        return {}
+      },
+    },
+  },
+  data() {
+    return {
+      ready: false,
+    }
+  },
+  beforeDestroy() {
+    this.parentContainer.removeLayer(this)
+    this.mapObject.remove()
+  },
+  mounted() {
+    this.mapObject = new MiniMap(this.layer, this.options)
+    DomEvent.on(this.mapObject, this.$listeners)
+    propsBinder(this, this.mapObject, this.$options.props)
+
+    this.ready = true
+    this.parentContainer = findRealParent(this.$parent)
+    this.mapObject.addTo(this.parentContainer.mapObject)
+
+    this.$nextTick(() => {
+      /**
+       * Triggers when the component is ready
+       * @type {object}
+       * @property {object} mapObject - reference to leaflet map object
+       */
+      this.$emit('ready', this.mapObject)
+    })
+  },
+  methods: {
+    changeLayer(layer) {
+      this.mapObject.changeLayer(layer)
+    },
+    minimize() {
+      this.mapObject.minimize()
+    },
+    restore() {
+      this.mapObject.restore()
+    },
+    toggle() {
+      this.mapObject.toggle()
+    },
+  },
+  render: function(h) {
+    if (this.ready && this.$slots.default) {
+      return h('div', { style: { display: 'none' } }, this.$slots.default)
+    }
+    return null
+  },
+}
+</script>
